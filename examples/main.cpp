ab4875ea9af2d7c2a2274b14b79e49c65462b93e
Anders Roos <anders.roos@gmail.com>
2017-01-28 18:28:32 +0100

    Added main program for devel testing.

---
+++ examples/main.cpp
@@ -0,0 +1,15 @@
+
+#include <atomic>
+#include <memory>
+#include <glo.hpp>
+
+//
+// Simple program to use  during development.
+//
+
+using namespace std;
+using namespace glo;
+
+int main()
+{
+}
